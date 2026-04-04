e0e542c511ac3063c9434bc39b1c2f54c95dc15d
alescarpino <alessandro.scarpino@edu.unifi.it>
2026-04-04 16:24:13 +0200

    first commit added header section(both for patient and doctor) added doctor info page for the patient

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -6,10 +6,12 @@ import { App } from './app';
 import { Doctor } from './doctor/doctor';
 import { Patient } from './patient/patient';
 import { Login } from './login/login';
-import { HomePage } from './patient/pages/home-page/home-page';
+//import { HomePage } from './patient/pages/home-page/home-page';
+import { HeaderComponent } from './shared/header/header.component';
+import { DoctorInfoComponent } from './patient/doctor-info/doctor-info.component';
 
 @NgModule({
-  declarations: [App, Doctor, Patient, Login, HomePage],
+  declarations: [App, Doctor, Patient, Login, HeaderComponent,DoctorInfoComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
