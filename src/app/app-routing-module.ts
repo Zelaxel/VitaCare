ae989d9abae3d64cfebab4854343b37a9cd7f7cc
Zelaxel <alejandroramirezlorenzo2003@gmail.com>
2026-04-05 21:19:39 +0100

    Refactor paths.

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -3,12 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { Doctor } from "./doctor/doctor";
 import {Patient} from './patient/patient';
 import {Login} from './login/login';
+import {PatientHomepage} from './patient/pages/homepage/patient-homepage';
+import {DoctorHomepage} from './doctor/pages/homepage/doctor-homepage';
 
 const routes: Routes = [
-  {path: 'patient', component: Patient},
-  {path: 'doctor', component: Doctor},
+  {
+    path: 'patient', component: Patient,
+    children: [
+      { path: 'home', component: PatientHomepage }
+    ]
+  },
+  {
+    path: 'doctor', component: Doctor,
+    children: [
+      { path: 'home', component: DoctorHomepage }
+    ]
+  },
   {path: 'login', component: Login},
-  {path: '', redirectTo: '/doctor', pathMatch: 'full'}
+  {path: '', redirectTo: '/patient/home', pathMatch: 'full'}
 ];
 
 @NgModule({
