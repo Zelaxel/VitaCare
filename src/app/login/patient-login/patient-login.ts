902c36b1dec093122cc3ab20170c48aeaf3de8e7
TheRubils <rubengc2120@gmail.com>
2026-04-18 10:58:20 +0100

    TypeScript for Register (done)

--- src/app/login/patient-login/patient-login.ts
+++ src/app/login/patient-login/patient-login.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'app-patient-login',
-  imports: [RouterLink],
+  imports: [FormsModule, RouterLink, CommonModule],
   templateUrl: './patient-login.html',
   styleUrl: './patient-login.css',
 })
-export class PatientLogin {}
+export class PatientLogin {
+  public id: string = "";
+  public password: string = "";
+  private idRegex: RegExp = /^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$/;
+  private passwordRegex: RegExp = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+  public loginErrorMessage: string = "";
+
+  constructor(private router: Router){}
+
+  get isIdInvalid(): boolean {
+    return this.id.length > 0 && !this.idRegex.test(this.id.toUpperCase());
+  }
+
+  get isPasswordInvalid(): boolean {
+    return this.password.length > 0 && !this.passwordRegex.test(this.password);
+  }
+
+  onLogin(){
+    this.loginErrorMessage = "";
+    if (this.idRegex.test(this.id) && this.passwordRegex.test(this.password)) {
+      this.router.navigate(['/patient']);
+    } else {
+      this.loginErrorMessage = "The data provided is invalid. Please check the fields in red.";
+      setTimeout(() => this.loginErrorMessage = "", 5000);
+    }
+  }
+} 
--- src/app/login/register/register.ts
+++ src/app/login/register/register.ts
@@ -1,17 +1,124 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
+  imports: [FormsModule, RouterLink, CommonModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
-  patientName: string = "patient";
-  doctorName: string = "doctor";
-  date: string = "date";
-  department: string = "department";
-  reason: string = "Lorem ipsum dolor sit amet consectetur adipiscing elit, nostra velit aliquam id habitant neque parturient, inceptos nascetur urna pharetra curae hendrerit. Habitant nisi rhoncus commodo urna leo vehicula fusce libero sagittis porttitor massa luctus turpis auctor in, per orci morbi eros eget proin convallis tristique dapibus et sodales ornare ut justo. Inceptos lobortis fringilla dignissim purus sem malesuada viverra lacinia laoreet accumsan, primis urna phasellus tellus senectus sagittis litora consequat ante. Leo et aenean aptent ante laoreet luctus, phasellus facilisi nec tempor auctor suscipit, euismod porta eget platea pretium.";
-  conclusion: string = "Lorem ipsum dolor sit amet consectetur adipiscing elit, nostra velit aliquam id habitant neque parturient, inceptos nascetur urna pharetra curae hendrerit. Habitant nisi rhoncus commodo urna leo vehicula fusce libero sagittis porttitor massa luctus turpis auctor in, per orci morbi eros eget proin convallis tristique dapibus et sodales ornare ut justo. Inceptos lobortis fringilla dignissim purus sem malesuada viverra lacinia laoreet accumsan, primis urna phasellus tellus senectus sagittis litora consequat ante. Leo et aenean aptent ante laoreet luctus, phasellus facilisi nec tempor auctor suscipit, euismod porta eget platea pretium.";
+  public id: string = "";
+  public calendarValue: string = "";
+  public email: string = "";
+  public telephoneNumber: string = ""; 
+  public healthCardNumber: string = "";
+  public password: string = "";
+  public confirmPassword: string = "";
+  private idRegex: RegExp = /^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$/;
+  private passwordRegex: RegExp = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  private emailRegex: RegExp = /^[a-zA-Z0-9._%+-]+@(gmail|hotmail)\.(com)$/;
+  private healthCardRegex: RegExp = /^[A-Z]{2,4}[0-9]{8,12}$/;
+  public loginErrorMessage: string = "";
+
+  constructor(private router: Router){}
+
+  public touchedFields: { [key: string]: boolean } = {
+    id: false,
+    email: false,
+    telephone: false,
+    card: false,
+    password: false,
+    confirmPassword: false,
+    date: false
+  };
+
+  get isIdInvalid(): boolean {
+    return this.touchedFields['id'] && (!this.idRegex.test(this.id.toUpperCase()) || this.id === "");
+  }
+
+  get isDateInvalid(): boolean {
+    return this.touchedFields['date'] && !this.calendarValue;
+  }
+
+
+  get isEmailInvalid(): boolean {
+    return this.touchedFields['email'] && (!this.emailRegex.test(this.email) || this.email === "");
+  }
+
+  get isTelephoneInvalid(): boolean {
+    if (!this.touchedFields['telephone']) return false;
+
+    const rawNumbers = this.telephoneNumber.replace('+34', '').replace(/\s/g, '');
+    const digitRegex = /^[0-9]{9}$/;
+
+    return rawNumbers === "" || !digitRegex.test(rawNumbers);
+  }
+
+  get isCardInvalid(): boolean {
+    return this.touchedFields['card'] && (!this.healthCardRegex.test(this.healthCardNumber.toUpperCase()) || this.healthCardNumber === "");
+  }
+
+  get isPasswordInvalid(): boolean {
+    return this.touchedFields['password'] && !this.passwordRegex.test(this.password);
+  }
+
+  get isConfirmPasswordInvalid(): boolean {
+    return this.touchedFields['confirmPassword'] && (this.password !== this.confirmPassword);
+  }
+  
+
+  onlyNumbers(event: any) {
+    const pattern = /[0-9]/;
+    const inputChar = String.fromCharCode(event.charCode);
+    if (event.keyCode !== 8 && !pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+  }
+
+  formatTelephone(event: any) {
+    let input = event.target.value;
+
+    if (!input.startsWith('+34 ')) {
+      input = '+34 ' + input.replace(/^\+34\s*/, '');
+    }
+
+    const prefix = "+34 ";
+    const numbersOnly = input.substring(prefix.length).replace(/\D/g, '');
+    const formattedNumbers = numbersOnly.replace(/(\d{3})(?=\d)/g, '$1 ');
+
+    this.telephoneNumber = prefix + formattedNumbers;
+    event.target.value = this.telephoneNumber;
+  }
+
+  markAsTouched(field: string): void {
+    this.touchedFields[field] = true;
+  }
+
+  onCalendarChange(event: any): void {
+    this.calendarValue = event.target.value; 
+    this.markAsTouched('date');
+  }
+
+  onRegister() {
+    // Importante: Al dar click en registrar, marcamos TODO como tocado
+    Object.keys(this.touchedFields).forEach(key => this.touchedFields[key] = true);
+
+    const isFormValid = 
+      !this.isIdInvalid && 
+      !this.isPasswordInvalid && 
+      !this.isConfirmPasswordInvalid && 
+      this.password !== "" &&           // Que no esté vacío
+      this.confirmPassword !== "" &&    // Que la confirmación tampoco esté vacía
+      this.password === this.confirmPassword; // Doble check de seguridad
+
+    if (isFormValid) {
+      this.router.navigate(['/login/log-in']);
+    } else {
+      this.loginErrorMessage = "Please complete all fields correctly.";
+      setTimeout(() => this.loginErrorMessage = "", 5000);
+    }
+  }
 }
