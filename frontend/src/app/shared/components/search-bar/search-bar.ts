498a76e673cfb9d706b9648e788e316154cd6cf5
alescarpino <alessandro.scarpino@edu.unifi.it>
2026-04-30 17:36:03 +0200

    implementado search bar

--- frontend/src/app/shared/components/search-bar/search-bar.ts
+++ frontend/src/app/shared/components/search-bar/search-bar.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component,EventEmitter,Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'app-search-bar',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search-bar.html',
   styleUrl: './search-bar.css',
 })
-export class SearchBar {}
+export class SearchBar {
+  searchTerm: string = ''
+  @Output() searchChange = new EventEmitter<string>();
+
+  onSearchInput() {
+    this.searchChange.emit(this.searchTerm.toLowerCase()); 
+  }
+}
--- frontend/src/app/shared/pages/home/home.ts
+++ frontend/src/app/shared/pages/home/home.ts
@@ -1,14 +1,11 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { SearchBar } from '../../components/search-bar/search-bar';
 import { AppointmentData } from '../../components/appointment/appointmentData';
 import { AppointmentGrid } from '../../components/appointment-grid/appointment-grid';
 import { Header } from '../../components/header/header';
-import { Router, RouterLink } from '@angular/router';
+import { Router, RouterLink } from "@angular/router";
 import { AppointmentService } from '../../../services/appointment-service';
-import { PatientService } from '../../../services/patient-service';
-import { AppointmentData as BackendAppointmentData } from '../../../model/appointment';
-import Swal from 'sweetalert2';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
@@ -17,182 +14,75 @@ import Swal from 'sweetalert2';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home implements OnInit {
-  appointments: AppointmentData[] = [];
-  departments: string[] = [];
-  patientData: any = null;
-
+export class Home {
+  allAppointments: AppointmentData[] = []; //real appointment grid
+  appointments: AppointmentData[] = []; //list we use for the filter
+  departments: string[] = ['Cardiology', 'Neurology', 'Pediatrics', 'Laboratory', 'General']; //test
   constructor(
     private router: Router,
-    private appointmentService: AppointmentService,
-    private patientService: PatientService,
-    private cdr: ChangeDetectorRef
+    private appointmentService: AppointmentService
   ) {}
-
+  
   isPatient(): boolean {
     return this.router.url.includes('/patient/home');
   }
-
-  isDoctor(): boolean {
-    return this.router.url.includes('/doctor/home');
-  }
-
-  ngOnInit(): void {
-    if (this.isDoctor()) {
-      this.loadDoctorAppointments();
+  ngOnInit() {
+    const userID = this.isPatient() 
+      ? localStorage.getItem('identity_document') 
+      : localStorage.getItem('credentials');
+      if (!userID) {
+      console.error("No user found!");
+      return; 
     }
 
     if (this.isPatient()) {
-      this.loadPatientAppointments();
-      this.loadDepartments();
-      this.checkPatientProfile();
-    }
-  }
-
-  loadDepartments() {
-    fetch('http://localhost:8000/departments')
-      .then(res => res.json())
-      .then(data => {
-        console.log("Departments:", data);
-        this.departments = data;
-        this.cdr.detectChanges();
-      });
-  }
-
-  loadDoctorAppointments(): void {
-    const doctorSurname = localStorage.getItem('doctor_surname');
-    const doctorId = localStorage.getItem('credentials');
-
-    if (!doctorSurname) {
-      console.error('Doctor surname not found');
-      return;
-    }
-
-    if(!doctorId){
-      console.error("doctor id not found");
-      return;
-    }
-
-    console.log(doctorId);
-    this.appointmentService.getAppointmentByDoctor(doctorId).subscribe({
-      next: (data: BackendAppointmentData[]) => {
-        const sortedData = this.sortAppointments(data);
-        this.appointments = data.map((appointment) =>
-          this.mapAppointmentToCard(appointment, false)
-        );
-        this.cdr.detectChanges();
-      },
-      error: (err) => {
-        console.error('Error loading doctor appointments', err);
-      }
-    });
-  }
-
-  loadPatientAppointments(): void {
-    const patientId = localStorage.getItem('identity_document');
-
-    if (!patientId) {
-      console.error('Patient identity document not found');
-      return;
-    }
-
-    this.appointmentService.getAppointmentByPatient(patientId).subscribe({
-      next: (data: BackendAppointmentData[]) => {
-        this.appointments = data.map((appointment) =>
-          this.mapAppointmentToCard(appointment, true)
-        );
-        this.cdr.detectChanges();
-      },
-      error: (err) => {
-        console.error('Error loading patient appointments', err);
-      }
-    });
-  }
-
-  checkPatientProfile(): void {
-    const patientId = localStorage.getItem('identity_document');
-    if (patientId) {
-      this.patientService.getPatient(patientId).subscribe({
-        next: (data) => {
-          this.patientData = data;
-        }
-      });
-    }
-  }
-
-  handleCreateAppointment(): void {
-    if (!this.patientData || !this.patientData.name || !this.patientData.surname) {
-      Swal.fire({
-        title: 'Profile Incomplete',
-        text: 'Please complete your name and surname in your profile before requesting an appointment.',
-        icon: 'warning',
-        confirmButtonText: 'Go to Profile',
-        confirmButtonColor: '#3085d6',
-        allowOutsideClick: false
-      }).then((result) => {
-        if (result.isConfirmed) {
-          // Solo redirige cuando el usuario hace clic en el botón
-          this.router.navigate(['/patient/user-profile']);
-        }
+      // if patient 
+      this.appointmentService.getAppointmentByPatient(userID).subscribe((data: any[]) => {
+        console.log("apoointment", data);
+        const datiPerGrafica = data.map(visit => ({
+          ...visit,
+          doctorName: visit.doctorName || 'Doctor', 
+          doctorIcon: visit.doctorIcon || '👤',
+          patientName: visit.patientName || 'Patient',
+          patientIcon: visit.patientIcon || '👤'
+        }));
+        this.allAppointments = data; 
+        this.appointments = data;
       });
     } else {
-      // Si todo está bien, navegar a la creación de cita
-      this.router.navigate(['/patient/create-appointment']);
+      // if doctor
+      this.appointmentService.getAppointmentByDoctor(userID).subscribe((data: any[]) => {
+        console.log("appointment", data);
+        const datiPerGrafica = data.map(visit => ({
+          ...visit, 
+          doctorName: visit.doctorName || 'Doctor', 
+          doctorIcon: visit.doctorIcon || '👤',
+          patientName: visit.patientName || 'Patient',
+          patientIcon: visit.patientIcon || '👤'
+        }));
+        this.allAppointments = data; 
+        this.appointments = data;    
+      });
     }
   }
-
-  private mapAppointmentToCard(
-    appointment: BackendAppointmentData,
-    patientLayout: boolean
-  ): AppointmentData {
-    return {
-      id: appointment.id ?? 0,
-      id_patient: appointment.id_patient,
-      id_doctor: appointment.id_doctor,
-      title: appointment.title,
-      doctorName: appointment.id_doctor,
-      doctorIcon: '🩺',
-      patientName: appointment.id_patient,
-      patientIcon: '👤',
-      description: appointment.reason,
-      date: appointment.attendance_date,
-      department: appointment.department,
-      active: appointment.active,
-    };
-  }
-
-
-
-
-  private sortAppointments(data: BackendAppointmentData[]): BackendAppointmentData[] {
-    return data.sort((a, b) => {
-      const timeA = new Date(a.attendance_date).getTime();
-      const timeB = new Date(b.attendance_date).getTime();
-
-      const isAActive = a.active;
-      const isBActive = b.active;
-
-      // SCENARIO 1: 'a' is active (to do), 'b' is inactive (done) -> 'a' comes first
-      if (isAActive && !isBActive) {
-        return -1; 
-      }
-      
-      // SCENARIO 2: 'b' is active (to do), 'a' is inactive (done) -> 'b' comes first
-      if (!isAActive && isBActive) {
-        return 1; 
-      }
-
-      // SCENARIO 3: Both appointments are active (To do)
-      if (isAActive && isBActive) {
-        // ASCENDING ORDER: From the closest upcoming date to the furthest in the future
-        return timeA - timeB; 
-      } 
-      
-      // SCENARIO 4: Both appointments are inactive (Done)
-      else {
-        // DESCENDING ORDER: From the most recently completed to the oldest one
-        return timeB - timeA; 
-      }
+  onSearch(SearchedText: string) {
+    // empty text
+    if (!SearchedText) {
+      this.appointments = [...this.allAppointments]; 
+      return; 
+    }
+    this.appointments = this.allAppointments.filter(visit => {
+      const titleMatch = visit.title.toLowerCase().includes(SearchedText);
+      return titleMatch;
     });
   }
+  loadDepartments() {
+    //department logic
+  }
+
+  handleCreateAppointment() {
+    this.router.navigate(['/patient/create-appointment']);
+  }
 }
+  
+
