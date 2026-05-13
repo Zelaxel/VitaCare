3e6125f46a30a8f99e8cab2b10bc58d8da456291
alescarpino <alessandro.scarpino@edu.unifi.it>
2026-05-13 16:08:26 +0200

    terminado conexion html ts, correciòn css

--- frontend/src/app/login/lost-account/email-sent.ts
+++ frontend/src/app/login/lost-account/email-sent.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router'
 
 @Component({
   standalone: true,
   selector: 'app-email-sent',
+  imports: [RouterLink],
   templateUrl: './email-sent.html',
   styleUrl: './email-sent.css',
 })
